bc4ef7805efc45150f09753e676f6b1f57b47e33
Maxvidor <maxim.dp.dorofeev@gmail.com>
2023-03-25 02:27:25 +0300

    add task4

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         task1 ();
         task2 ();
         task3 ();
+        task4 ();
 
     }
     public static void task1 () {
@@ -37,6 +38,27 @@ public class Main {
             System.out.println(year + " год является високосным");
         } else {
             System.out.println(year + " год не является високосным ");
+        }
+    }
+    public static void task4 () {
+        System.out.println("Задача 4");
+        int deliveryDistance = 95;
+        int deliveryTime;
+
+        if (deliveryDistance <= 20) {
+            deliveryTime = 1;
+        } else if (deliveryDistance <= 60) {
+            deliveryTime = 2;
+        } else if (deliveryDistance <= 100) {
+            deliveryTime = 3;
+        } else {
+            deliveryTime = 0;
+        }
+
+        if (deliveryTime > 0) {
+            System.out.println("Потребуется дней: " + deliveryTime);
+        } else {
+            System.out.println("Доставки нет");
         }
     }
 }
