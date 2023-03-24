4ebe2727e72a6d9d91333d28827fb44dac99d79f
Maxvidor <maxim.dp.dorofeev@gmail.com>
2023-03-25 00:33:46 +0300

    add task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1 ();
+
+    }
+    public static void task1 () {
+        System.out.println("Задача 1");
+        char ClientOS = 1;
+        if(ClientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (ClientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else
+            System.out.println("Ваше мобильное устройство не поддерживает мобильное приложение");
+
     }
 }
